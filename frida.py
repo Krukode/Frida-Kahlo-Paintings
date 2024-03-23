bc63d7891f78077f7e526f43d09cd3a685f6eeb7
Krucode <bart.kawalec@gmail.com>
2024-03-23 21:16:51 +0000

    Implemented the functions

--- frida.py
+++ frida.py
@@ -1,33 +1,27 @@
 # This python project creates a list of Frida Kahlo paintings, their dates and the spot in the exhibition tour.
 
-# Create a list of paintings
-paintings = ['The Two Fridas', 'My Dress Hangs Here', 'Tree of Hope', 'Self Portrait With Monkeys']
+def create_paintings_list():
+    # List of paintings
+    paintings = ['The Two Fridas', 'My Dress Hangs Here', 'Tree of Hope', 'Self Portrait With Monkeys']
+    # Corresponding dates of the paintings
+    dates = [1939, 1933, 1946, 1940]
+    # Returns a list containing painting names and their dates
+    return list(zip(paintings, dates))  
 
-# Create a list of dates
-dates = [1939, 1933, 1946, 1940]
+def generate_audio_tour_numbers(start, end):
+    # Generates a list of sequential numbers for audio tour identifiers
+    return list(range(start, end))
 
-# Zip together the lists above
-paintings = list(zip(paintings, dates))
+def combine_data(audio_tour_numbers, paintings):
+    # Combines the audio tour numbers with the paintings
+    return list(zip(audio_tour_numbers, paintings))  # Returns a list containing audio tour numbers and painting information
 
-# Add new paintings to the list
-paintings.append(('The Broken Column', 1944))
-paintings.append(('The Wounded Deer', 1946))
-paintings.append(('The Wounded Deer', 1937))
-
-print(paintings)
-
-# Find the lenght of the paintings list
-len(paintings)
-
-# Generate and print  a list of identification numbers
-audio_tour_number = list(range(1, 8))
-
-print(audio_tour_number)
-
-# Create and print a list that combines the paintings, dates and id number
-master_list = list(zip(audio_tour_number, paintings))
-
-print(master_list)
+# Usage
+paintings = create_paintings_list()  # Creates a list of paintings with their corresponding dates
+audio_tour_numbers = generate_audio_tour_numbers(1, len(paintings) + 1)  # Generates audio tour numbers for the paintings
+master_list = combine_data(audio_tour_numbers, paintings)  # Combines audio tour numbers with paintings
+# Prints the master list containing tuples of audio tour numbers and painting information
+print(master_list)  
 
 
 
